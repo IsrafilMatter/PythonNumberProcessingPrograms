feac966db33d4446101785260a36a86020e70b50
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 00:23:10 +0800

    pseudocode

---
+++ calculate_number_power.py
@@ -0,0 +1,6 @@
+# Print the result when the first number is raised to the second number
+# Author: Israfil Palabay
+# Date: March 12, 2024
+
+# Ask the user to input 2 numbers
+# Raised it to the second number and print
