9ea2fd2cef1061f973bc6dc1ddd7b2148476928b
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-11 23:40:26 +0800

    Ask the user to input 2 numbers

--- find_bigger_number.py
+++ find_bigger_number.py
@@ -3,4 +3,7 @@
 # Date: March 11, 2024
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Compare and Print the bigger number
