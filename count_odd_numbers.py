c8433c261ca6fb47bc6f29ce7e6400bef31cf76c
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 13:26:01 +0800

    Ask the user to input 10 numbers

--- count_odd_numbers.py
+++ count_odd_numbers.py
@@ -3,6 +3,8 @@
 # Date: March 12, 2024
 
 # Create a list of 10 numbers inputted by the user
+numbers = [float(input(f'Enter number {i + 1}: ')) for i in range(10)]
+
 # Count how many numbers in the list are odd
 # Since float numbers may have decimals, we cast them to integers before checking odd/even
 # Print the total count of odd numbers
