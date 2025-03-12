a2868a1cfddc38e585a946c90c354017a5f431fe
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 13:22:38 +0800

    Function determining the 2 numbers if it is equal

--- check_number_equality.py
+++ check_number_equality.py
@@ -7,3 +7,7 @@ first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 # Comparing the 2 numbers if it is Equal
+if first_number == second_number:
+    print('Equal')
+else:
+    print('Not Equal')
