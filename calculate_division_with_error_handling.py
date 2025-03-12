dbbfc656b32e413255636c5d183bd52cfaa63e63
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 10:18:39 +0800

    Ask the user to input 2 numbers

--- calculate_division_with_error_handling.py
+++ calculate_division_with_error_handling.py
@@ -3,6 +3,9 @@
 # Date: March 12, 2024
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Check if the denominator is not zero to avoid by zero error
 # Calculate and print the quotient if division is possible
 # Print an error message if division by zero is attempted
