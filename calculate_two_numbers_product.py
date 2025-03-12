9764af6571f99d2df9d764730a7db0d2cb612a64
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 10:15:45 +0800

    Getting the product and print the results

--- calculate_two_numbers_product.py
+++ calculate_two_numbers_product.py
@@ -7,3 +7,4 @@ first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 # Get the product of the 2 numbers and print
+print(f'The product is: {first_number * second_number}')
