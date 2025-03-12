66bacd2e2e6bf3e4dc555b32ea0855be6d9b4455
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 10:21:42 +0800

    Ask the user to input 2 numbers

--- check_number_equality.py
+++ check_number_equality.py
@@ -1,6 +1,9 @@
-# check_number_equality
+# Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 # Author: Israfil Palabay
 # Date: March 11, 2024
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Comparing the 2 numbers if it is Equal
