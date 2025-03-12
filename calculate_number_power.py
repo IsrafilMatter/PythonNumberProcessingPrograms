019381a506710d6569e645cc897c572b3d32e114
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 10:19:53 +0800

    Ask the user to input 2 numbers

--- calculate_number_power.py
+++ calculate_number_power.py
@@ -3,4 +3,7 @@
 # Date: March 12, 2024
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Raised it to the second number and print
