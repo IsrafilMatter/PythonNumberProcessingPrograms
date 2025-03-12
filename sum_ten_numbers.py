c2bc9558408256e74b2a788ef6796ee6b0a5f858
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 13:47:29 +0800

    Ask the user to input 10 numbers

--- sum_ten_numbers.py
+++ sum_ten_numbers.py
@@ -3,4 +3,5 @@
 # Date: March 12, 2024
 
 # Create a list of 10 numbers inputted by the user
+numbers = [float(input(f'Enter number {index + 1}: ')) for index in range(10)]
 # Calculate and print the sum of all numbers in the list
