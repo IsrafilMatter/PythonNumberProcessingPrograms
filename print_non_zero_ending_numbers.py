eb00274226c99781d1c22527d724849358ed567f
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 14:03:16 +0800

    Looping through from 0 to 100 and check if the number does not end with zero and print the result

--- print_non_zero_ending_numbers.py
+++ print_non_zero_ending_numbers.py
@@ -3,5 +3,6 @@
 # Date: March 12, 2024
 
 # Loop through numbers from 0 to 100
-# Check if the number does not end in zero
-# Print the number if the condition is met
+for index in range(101):
+    if index % 10 != 0: # Check if the number does not end in zero
+        print(index)    # Print the number if the condition is met
