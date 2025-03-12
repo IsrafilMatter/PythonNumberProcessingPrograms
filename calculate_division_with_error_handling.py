ee6345095a3963f07a87e1ee7babc74a6dfeb6d0
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 12:55:00 +0800

    SyntaxError: unterminated f-string literal at line 11

--- calculate_division_with_error_handling.py
+++ calculate_division_with_error_handling.py
@@ -7,5 +7,7 @@ first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 # Check if the denominator is not zero to avoid by zero error
-# Calculate and print the quotient if division is possible
-# Print an error message if division by zero is attempted
+if first_number != 0 and second_number != 0:
+    print(f'The quotient is: {first_number / second_number}) # Calculate and print the quotient if division is possible
+else:
+    print('Cannot divide by zero') # Print an error message if division by zero is attempted
