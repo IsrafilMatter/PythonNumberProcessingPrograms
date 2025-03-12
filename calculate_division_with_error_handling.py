a6658408b17649f13d4efee8c57704ea94849214
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 13:09:21 +0800

    Error Fixed: the '' was missing at line 11

--- calculate_division_with_error_handling.py
+++ calculate_division_with_error_handling.py
@@ -8,6 +8,6 @@ second_number = float(input("Enter the second number: "))
 
 # Check if the denominator is not zero to avoid by zero error
 if first_number != 0 and second_number != 0:
-    print(f'The quotient is: {first_number / second_number}) # Calculate and print the quotient if division is possible
+    print(f'The quotient is: {first_number / second_number}') # Calculate and print the quotient if division is possible
 else:
     print('Cannot divide by zero') # Print an error message if division by zero is attempted
