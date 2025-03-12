15e5d42485c72dcff907651ff4e59f86a415c91e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-12 13:14:34 +0800

    fixing the space in between the * to be able to follow the standard in python

--- calculate_two_numbers_product.py
+++ calculate_two_numbers_product.py
@@ -7,4 +7,4 @@ first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 # Get the product of the 2 numbers and print
-print(f'The product is: {first_number * second_number}')
+print(f'The product is: {first_number*second_number}')
