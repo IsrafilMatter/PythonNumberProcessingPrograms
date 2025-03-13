ccddab034d7c899fa31f1bd559044c3c6865d849
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:32:19 +0800

    pseudocode

---
+++ calculate_division_remainder.py
@@ -0,0 +1,6 @@
+# Print the remainder when the first number is divided by the second number
+# Author: Israfil Palabay
+# Date: March 12, 2024
+
+# Ask the user to input 2 numbers
+# Check if second_number is zero to avoid division by zero error
