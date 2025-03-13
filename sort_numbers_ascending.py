edba6265f31f94387b35ee324b4d52793f0cf4ab
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 22:35:53 +0800

    pseudocode

---
+++ sort_numbers_ascending.py
@@ -0,0 +1,9 @@
+# Program that continuously asks for numbers until invalid input and sorts them in ascending order
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+# Use a loop to keep asking for user input
+# If input is numeric, store it in the list
+# If input is invalid, break the loop
+# If the list is not empty: Sort and display numbers in ascending order
