58efc155061a7435f05dcb404289f764507feb69
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 13:03:11 +0800

    Ask the user to input 2 numbers

--- find_smaller_number.py
+++ find_smaller_number.py
@@ -1,6 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
 # Author: Israfil Palabay
-# Date: March 12, 2024
+# Date: March 12, 2025
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Compare and Print the smaller number
