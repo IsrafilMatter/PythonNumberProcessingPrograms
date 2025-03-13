aa5901b0d5bff06cab32745620290b67be1969f0
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 21:31:40 +0800

    pseudocode

---
+++ find_highest_number.py
@@ -0,0 +1,9 @@
+# Program that continuously asks for numbers until invalid input and finds the highest number
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+# Use a loop to keep asking for user input
+# If input is numeric, store it in the list
+# If input is invalid, break the loop
+# If the list is not empty, find and print the highest number
