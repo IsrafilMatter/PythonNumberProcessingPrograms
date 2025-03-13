8e17e6c013d2ac5749b781c50e991ded77e415b4
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 14:05:11 +0800

    initializing and get the user to input 10 numbers

--- subract_numbers_from_first.py
+++ subract_numbers_from_first.py
@@ -1,8 +1,13 @@
 # Print the result of the first number minus all of the remaining numbers
 # Author: Israfil Palabay
-# Date: March 13, 2024
+# Date: March 13, 2025
 
 # Initialize an empty list
-# Ask the user to input 2 numbers
-# Ensure the list has at least one number before performing calculations
+numbers = []
 
+# Get input from the user
+for index in range(10):
+    num = float(input(f'Enter number {index + 1}: '))
+    numbers.append(num)
+    
+# Ensure the list has at least one number before performing calculations
