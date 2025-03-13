a6e2bb65f2bc7a05d5a5128d080f948b9c52bea9
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 15:11:33 +0800

    Initializing and the 1st odd start with 1 and using while loop to print only the odd numbers from 0 to 100

--- print_odd_numbers_to_100.py
+++ print_odd_numbers_to_100.py
@@ -2,5 +2,10 @@
 # Author: Israfil Palabay
 # Date: March 13, 2025
 
-# Start from 1, the first odd number
-# Increment by 2 to always get the next odd number
+# Initialize
+num = 1 # Start from 1 since it is the first odd number
+
+# While loop
+while num <= 100:
+    print(num) # Print the odd numbers
+    num += 2 # Increment by 2 to always get the next odd number
