fa6f9944ad4b6c9a838574b0220cc8d5e53b8c8e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:26:09 +0800

    Did not save and push the right pseudocode, kindly this regard the first pseudocode committed

---
+++ calculate_number_difference.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+# Author: Israfil Palabay
+# Date: March 12, 2024
+
+# Ask the user to input 2 numbers
+# Get the difference and Print the results
---
+++ check_number_inequality.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# Author: Israfil Palabay
+# Date: March 13, 2024
+
+# Ask the user to input 2 numbers
+# Check for inequality and print
