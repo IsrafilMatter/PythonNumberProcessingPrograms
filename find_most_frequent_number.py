26233a025cee79b55684cde5ebe0cac2d6b97c29
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 21:24:21 +0800

    pseudocode

---
+++ find_most_frequent_number.py
@@ -0,0 +1,7 @@
+# Program that continuously asks for numbers until invalid input and finds the most frequent number
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+# Ask the user to input number and Enter any non-numeric value to stop
+# Find the most frequent number
