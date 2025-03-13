5be269b039ef0689568ea8e87b7cfa4912e35251
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 14:52:09 +0800

    SyntaxError again

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -11,5 +11,5 @@ for index in range(10):
     numbers.append(num)
     
 # Count even numbers 
-even_count = sum(1 for num index in numbers if num % 2 == 0)
-print('Numbers of even numbers:', even_count) # Print the result
+even_count = sum(1 for num index in numbers if int(num) % 2 == 0)
+print('Numbers of even numbers: even_count')# Print the result
