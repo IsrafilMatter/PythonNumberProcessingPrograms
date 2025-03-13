a99ff83f333681bd5e63885c9551d48aa0f086e8
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 14:25:54 +0800

    Initializing and get the user to input 10 numbers

--- count_even_numbers.py
+++ count_even_numbers.py
@@ -1,7 +1,14 @@
 # Print how many of the 10 input numbers are even
 # Author: Israfil Palabay
-# Date: March 13, 2024
+# Date: March 13, 2025
+
+# Initialize
+numbers = []
 
 # Get input from the user
+for index in range(10):
+    num = float(input(f'Enter number {index + 1}: '))
+    numbers.append(num)
+    
 # Count even numbers (casting to int first)
 # Print the result
