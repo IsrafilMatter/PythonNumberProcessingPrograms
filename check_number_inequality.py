d8569d97a3357c2a94a085a3bc9beaf329015e05
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 13:19:10 +0800

    Ask the user to input 2 numbers

--- check_number_inequality.py
+++ check_number_inequality.py
@@ -1,6 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 # Author: Israfil Palabay
-# Date: March 13, 2024
+# Date: March 13, 2025
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Check for inequality and print
