76b64a9a92017b8bf41f9d1ed1af4241810d73e7
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 13:30:17 +0800

    Ask the user to input 2 numbers

--- calculate_number_difference.py
+++ calculate_number_difference.py
@@ -1,6 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 # Author: Israfil Palabay
-# Date: March 12, 2024
+# Date: March 13, 2025
 
 # Ask the user to input 2 numbers
+first_number = float(input("Enter the first number: "))
+second_number = float(input("Enter the second number: "))
+
 # Get the difference and Print the results
