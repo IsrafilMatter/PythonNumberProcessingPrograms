887f061f61580419c0af23b98122f074c5a4399a
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 20:52:48 +0800

    pseudocode

---
+++ find_lowest_number.py
@@ -0,0 +1,9 @@
+# Program that continuously asks for numbers until invalid input and finds the lowest number
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize, Creating an empty list to store numbers
+# Use a loop to keep asking for user input
+# If input is numeric, store it in the list
+# If input is invalid, break the loop
+# If the list is not empty, find and print the lowest number
