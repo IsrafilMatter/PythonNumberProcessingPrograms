dc99e75890abbc6ca4b81c03ef3a1cdd70eccaa6
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 19:49:49 +0800

    pseudocode

---
+++ display_first_occurrence.py
@@ -0,0 +1,7 @@
+# Program that asks user to input 10 numbers and displays first occurrence of each number
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+# Get 10 numbers from user
+# Display first occurrence of each number
