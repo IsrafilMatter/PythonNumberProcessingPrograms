9201fe4ac5501cfac16180d609263d08d001bef9
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 13:17:34 +0800

    Comparing the number inputted and print the smaller number

--- find_smaller_number.py
+++ find_smaller_number.py
@@ -1,9 +1,15 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
 # Author: Israfil Palabay
-# Date: March 12, 2025
+# Date: March 13, 2025
 
 # Ask the user to input 2 numbers
 first_number = float(input("Enter the first number: "))
 second_number = float(input("Enter the second number: "))
 
 # Compare and Print the smaller number
+if first_number > second_number:
+    print(f'The bigger number is: {first_number}')
+elif first_number < second_number:
+    print(f'The bigger number is: {second_number}')
+else:
+    print('Both numbers are equal')
