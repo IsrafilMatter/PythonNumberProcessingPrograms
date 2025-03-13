1b5c0bac0b462bffa641bcfb01138071f6b9c18a
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 21:03:24 +0800

    pseudocode

---
+++ display_duplicate_numbers.py
@@ -0,0 +1,8 @@
+# Program that asks user to input 10 numbers and displays numbers that have duplicates
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+# Get 10 numbers from user
+# Find numbers with duplicates
+# Display numbers with duplicates
