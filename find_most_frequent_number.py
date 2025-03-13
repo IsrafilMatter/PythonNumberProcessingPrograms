70fa4be473a8ecede38f1e7bb5e3007b50ae8bc3
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 21:25:13 +0800

    Initializing

--- find_most_frequent_number.py
+++ find_most_frequent_number.py
@@ -3,5 +3,8 @@
 # Date: March 13, 2025
 
 # Initialize
+numbers = []
+print("Enter numbers (enter any non-numeric value to stop):")
+
 # Ask the user to input number and Enter any non-numeric value to stop
 # Find the most frequent number
