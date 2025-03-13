5472d3735fb81ac1c687882fe0e65d489222a310
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 20:56:50 +0800

    Initializing and asking for the user to enter a number in loop if when the user inputted a non-numeric value stop the program

--- find_lowest_number.py
+++ find_lowest_number.py
@@ -3,7 +3,17 @@
 # Date: March 13, 2025
 
 # Initialize, Creating an empty list to store numbers
+numbers = []
+print('Enter numbers (enter any non-numeric value to stop):')
+
 # Use a loop to keep asking for user input
+while True:
+    try: 
+        num = float(input('Enter a number: '))
+        numbers.append(num)
+    except ValueError:
+        break
+    
 # If input is numeric, store it in the list
 # If input is invalid, break the loop
 # If the list is not empty, find and print the lowest number
