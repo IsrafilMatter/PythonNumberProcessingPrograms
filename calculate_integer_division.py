791a86edf88685e4421991905aa1062111844db1
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 13:00:01 +0800

    pseudocode

---
+++ calculate_integer_division.py
@@ -0,0 +1,7 @@
+# Print the quotient of two numbers without the decimal point
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Get two numbers from the user.
+# Perform integer division to remove the decimal part.
+# Print the quotient
