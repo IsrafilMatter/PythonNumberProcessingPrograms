5eefc9a11aebe095a7b52db378a47921ef21e78e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:19:59 +0800

    pseudocode

---
+++ find_smaller_number.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input 2 numbers. Print the smaller number.
+# Author: Israfil Palabay
+# Date: March 12, 2024
+
+# Ask the user to input 2 numbers
+# Compare and Print the smaller number
