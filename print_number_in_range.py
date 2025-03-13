d0304e66a9b016693289f329dfebe1123e3901bb
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 15:26:18 +0800

    Get two numbers from user

--- print_number_in_range.py
+++ print_number_in_range.py
@@ -3,6 +3,9 @@
 # Date: March 13, 2025
 
 # Get two numbers from the user
+first_number = int(input('Enter the first number: '))
+second_number = int(input('Enter the second number: '))
+                    
 # Determine the smaller and larger number to handle any order of input
 # Loop through the numbers between them (excluding the input numbers)
 # Print each number
