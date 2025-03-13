a1d00212097227975a917636112424ce4d12a4bb
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:43:29 +0800

    pseudocode

---
+++ print_odd_numbers_to_100.py
@@ -0,0 +1,6 @@
+# Print all the odd numbers from 0 to 100 (using a while loop)
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Start from 1, the first odd number
+# Increment by 2 to always get the next odd number
