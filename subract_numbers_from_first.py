c27c377721fdbea0f0f78b02022ca183a3903128
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:35:39 +0800

    pseudocode

---
+++ subract_numbers_from_first.py
@@ -0,0 +1,8 @@
+# Print the result of the first number minus all of the remaining numbers
+# Author: Israfil Palabay
+# Date: March 13, 2024
+
+# Initialize an empty list
+# Ask the user to input 2 numbers
+# Ensure the list has at least one number before performing calculations
+
