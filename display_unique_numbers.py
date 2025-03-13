e0adb3f2f9473973d3d037c286978a2b45bf228e
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 19:36:16 +0800

    Print to ask the user to enter 10 numbers

---
+++ display_unique_numbers.py
@@ -0,0 +1,11 @@
+# Program that asks user to input 10 numbers and displays numbers without duplicates
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Initialize
+numbers = []
+print('Enter 10 numbers: ')
+
+# Get 10 numbers from user
+# Find unique numbers
+# Display unique numbers
