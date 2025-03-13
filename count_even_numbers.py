6cf27888490ac00b1907968a4a52ccb75cb8cd5d
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:38:34 +0800

    pseudocode

---
+++ count_even_numbers.py
@@ -0,0 +1,7 @@
+# Print how many of the 10 input numbers are even
+# Author: Israfil Palabay
+# Date: March 13, 2024
+
+# Get input from the user
+# Count even numbers (casting to int first)
+# Print the result
