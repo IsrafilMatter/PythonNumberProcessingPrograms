7664c314866e6e4a13d87a49adc8878f57c5610c
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:56:11 +0800

    pseudocode

---
+++ print_number_in_range.py
@@ -0,0 +1,8 @@
+# Print all the numbers between two user-input numbers
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Get two numbers from the user
+# Determine the smaller and larger number to handle any order of input
+# Loop through the numbers between them (excluding the input numbers)
+# Print each number
