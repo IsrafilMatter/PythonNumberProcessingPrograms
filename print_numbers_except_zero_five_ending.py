b5ecf226ef51d4a3962475f49d40cfff6d1b2a80
IsrafilMatter <israfildayopalabay21@gmail.com>
2025-03-13 12:51:10 +0800

    pseudocode

---
+++ print_numbers_except_zero_five_ending.py
@@ -0,0 +1,7 @@
+# Print all numbers from 0 to 100 except those ending in zero or five
+# Author: Israfil Palabay
+# Date: March 13, 2025
+
+# Loop through numbers from 0 to 100
+# Check if the last digit is NOT 0 or 5 (i.e., num % 10 is not in {0, 5})
+# If the condition is met, print the number
